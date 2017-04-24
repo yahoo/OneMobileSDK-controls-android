a76c9c6bac1527b77f02b5c43a85cac3592fae31
Alexander Levadniy <aleksander.levadniy@teamaol.com>
2017-04-24 20:25:06 +0300

    Template param removal

--- src/main/java/com/aol/mobile/sdk/controls/PlayerControls.java
+++ src/main/java/com/aol/mobile/sdk/controls/PlayerControls.java
@@ -4,11 +4,12 @@ package com.aol.mobile.sdk.controls;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
+import android.view.View;
 
 import com.aol.mobile.sdk.controls.viewmodel.PlayerControlsVM;
 
 @UiThread
-public interface PlayerControls<View> {
+public interface PlayerControls {
     void setListener(@Nullable Listener listener);
 
     void render(@NonNull PlayerControlsVM viewModel);
