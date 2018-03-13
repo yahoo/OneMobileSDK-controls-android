9bf598b7f47ec0d8432ae0f904cb103b337c7844
AleksanderLevadniy <aleksander.levadniy@oath.com>
2018-03-13 13:27:18 +0200

    Copyright update

--- controls/src/test/java/com/aol/mobile/sdk/controls/view/AdControlsViewTest.kt
+++ controls/src/test/java/com/aol/mobile/sdk/controls/view/AdControlsViewTest.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2018, Oath Inc.
+ * Licensed under the terms of the MIT License. See LICENSE.md file in project root for terms.
+ */
+
 package com.aol.mobile.sdk.controls.view
 
 import com.aol.mobile.sdk.controls.AdControls
